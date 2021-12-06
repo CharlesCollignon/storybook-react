0c9a10e1aebbb803f9c0fd6fddb1c106fcf6cc1b
Charles Collignon <c.collignon@q3-technology.com>
2021-12-06 15:37:18 +0100

    modify stories & add fake DBs

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -4,7 +4,7 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Button from "./Button";
 
 export default {
-  title: "ReactComponentLibrary/Button",
+  title: "React Component Library/Button",
   component: Button,
 } as ComponentMeta<typeof Button>;
 
